c4642a86472f2942d87d020968a57861d68fd818
frfernandini <fr.fernandini@duocuc.cl>
2025-09-03 11:28:24 -0400

    validacion de formulario contacto y login

--- src/js/login.js
+++ src/js/login.js
@@ -1,9 +1,10 @@
+console.log("Archivo login.js cargado correctamente");
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
 window.addEventListener("DOMContentLoaded", () => {
     const form = document.querySelector("form");
     const correoInput = document.getElementById("CorreoInput");
     const passwordInput = document.getElementById("contrasenaInput");
+    const errorMessageDiv = document.getElementById("error-message");
     form?.addEventListener("submit", (e) => {
         const correo = correoInput.value.trim();
         const contrasena = passwordInput.value.trim();
@@ -11,10 +12,11 @@ window.addEventListener("DOMContentLoaded", () => {
         const validacionContrasena = (contrasena.length >= 4 && contrasena.length <= 10);
         if (validacionCorreo && validacionContrasena) {
             console.log("sesion iniciada");
+            window.location.href = "home.html";
         }
         else {
             console.log("informacion no valida");
+            errorMessageDiv.textContent = "Correo o contraseña no válidos. Por favor, inténtalo de nuevo.";
         }
     });
 });
-//# sourceMappingURL=login.js.map
