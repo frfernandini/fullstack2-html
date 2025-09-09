1a9aa02611da8c8e23e0871bf0437567a5f00b5d
franco <fr.fernandini@duocuc.cl>
2025-09-09 14:11:20 -0300

    cambiado productos estaticos y eliminado html innecesario

--- src/js/home.js
+++ src/js/home.js
@@ -1,32 +1,33 @@
 //CREANDO LISTADO DE PRODUCTOS
+//CREANDO LISTADO DE PRODUCTOS
 const productosEstaticos = {
     JM001: {
         Categoria: "Juegos de Mesa",
         Titulo: "Catan",
         Descripcion: "Un clásico juego de estrategia donde los jugadores compiten por colonizar y expandirse en la isla de Catan. Ideal para 3-4 jugadores y perfecto para noches de juego en familia o con amigos.",
         Precio: 29990,
-        imagen: "img/ps5-test.png"
+        imagen: "img/catan.png"
     },
     JM002: {
         Categoria: "Juegos de Mesa",
         Titulo: "Carcassonne",
         Descripcion: " Un juego de colocación de fichas donde los jugadores construyen el paisajealrededor de la fortaleza medieval de Carcassonne. Ideal para 2-5 jugadores y fácil deaprender.",
         Precio: 24990,
-        imagen: "img/ps5-test.png"
+        imagen: "img/carcassonne.png"
     },
     AC001: {
         Categoria: "Accesorios",
         Titulo: "Controlador Inalámbrico Xbox Series X",
         Descripcion: "Ofrece una experiencia de juego cómoda con botones mapeables y una respuesta táctil mejorada. Compatible con consolas Xbox y PC.",
         Precio: 59990,
-        imagen: "img/ps5-test.png"
+        imagen: "img/xbox_controller.png"
     },
     AC002: {
         Categoria: "Accesorios",
         Titulo: "Auriculares Gamer HyperX Cloud II",
         Descripcion: "Proporcionan un sonido envolvente de calidad con un micrófono desmontable y almohadillas de espuma viscoelástica para mayor comodidad durante largas sesiones de juego.",
         Precio: 79990,
-        imagen: "img/ps5-test.png"
+        imagen: "img/hyperx_cloud.png"
     },
     CO001: {
         Categoria: "Consolas",
@@ -40,7 +41,7 @@ const productosEstaticos = {
         Titulo: "PC Gamer ASUS ROG Strix",
         Descripcion: "Un potente equipo diseñado para los gamers más exigentes, equipado con los últimos componentes para ofrecer un rendimiento excepcional en cualquier juego",
         Precio: 1299990,
-        imagen: "img/ps5-test.png"
+        imagen: "img/pc gamer.png"
     },
     SG001: {
         Categoria: "Sillas Gamers",
@@ -54,21 +55,21 @@ const productosEstaticos = {
         Titulo: "Mouse Gamer Logitech G502 HERO",
         Descripcion: " Con sensor de alta precisión y botones personalizables, este mouse es ideal para gamers que buscan un control preciso y personalización",
         Precio: 49990,
-        imagen: "img/ps5-test.png"
+        imagen: "img/mouse.png"
     },
     MP001: {
         Categoria: "Mousepad",
         Titulo: "Mousepad Razer Goliathus Extended Chroma",
         Descripcion: "Ofrece un área de juego amplia con iluminación RGB personalizable, asegurando una superficie suave y uniforme para el movimiento del mouse.",
         Precio: 29990,
-        imagen: "img/ps5-test.png" 
+        imagen: "img/mousepad.png" 
     },
     PP001: {
         Categoria: "Poleras Personalizadas",
         Titulo: "Polera Gamer Personalizada 'Level-Up'",
         Descripcion: "Una camiseta cómoda y estilizada, con la posibilidad de personalizarla con tu gamer tag o diseño favorito",
         Precio: 14990,
-        imagen: "img/ps5-test.png" 
+        imagen: "img/polera_gamer_life.png" 
     }
 };
 
