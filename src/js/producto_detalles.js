2b943140745a5a6e6319c905d2a18f9380dba702
johao <joha.candia@duocuc.cl>
2025-09-09 19:02:07 -0300

    Eliminar codigo innecesario productos,Lista,detalles

--- src/js/producto_detalles.js
+++ src/js/producto_detalles.js
@@ -40,16 +40,4 @@ miniaturas.forEach(miniatura => {
 });
 
 
-/*Poder cambiar la imagen principal y las miniaturas
-    const mainImage = document.getElementById('mainImage');
-    const miniaturas = document.querySelectorAll('.miniatura');
 
-    miniaturas.forEach(miniatura => {
-        miniatura.addEventListener('click', function() {
-            // Guarda la ruta de la imagen principal actual
-            const tempSrc = mainImage.src;
-            // Intercambia la imagen principal con la miniatura clickeada
-            mainImage.src = this.src;
-            this.src = tempSrc;
-            });
-        });*/
