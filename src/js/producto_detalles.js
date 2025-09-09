1f55e8f9cfeb4ec250cbf88aa68311b67fb9bf03
johao <joha.candia@duocuc.cl>
2025-09-09 19:49:35 -0300

    Barra de productos relacionados funcional listo

--- src/js/producto_detalles.js
+++ src/js/producto_detalles.js
@@ -4,9 +4,12 @@ const productId = params.get('id');
 
 // Obtener todos los productos
 const todosLosProductos = { ...productosEstaticos, ...cargarProductosAgregados() };
+
+// Obtener el producto específico
 const producto = todosLosProductos[productId];
 
 const detalleDiv = document.getElementById('detalle-producto');
+const barraRelacionados = document.getElementById('productos-relacionados');
 
 if (producto) {
     detalleDiv.innerHTML = `
@@ -27,7 +30,7 @@ if (producto) {
     `;
 }
 
-// Función para cambiar la imagen principal al hacer click en una miniatura
+//cambiar la imagen principal por las miniaturas
 const mainImage = document.getElementById('mainImage');
 const miniaturas = document.querySelectorAll('.miniatura');
 
@@ -38,6 +41,44 @@ miniaturas.forEach(miniatura => {
         this.src = tempSrc;
     });
 });
+
+//Crear una lista vacia para almacenar productos relacionados
+let relacionados = [];
+
+// Recorrer todos los productos
+for (let id in todosLosProductos) {
+    let prod = todosLosProductos[id];
+
+    //Se condicionala en base a la categoria asegurandose de que no es el producto actualmente visto
+    if (prod.Categoria === producto.Categoria && id !== productId) {
+        relacionados.push({ id, ...prod });
+    }
+}
+
+// Tomar solo 6 productos relacionados
+relacionados = relacionados.slice(0, 6);
+
+// Mostrar los productos en la barra
+relacionadosBarra.innerHTML = "";
+for (let rel of relacionados) {
+    // Se crea un elemento de tipo div
+    let div = document.createElement("div");
+    //se implementan una clase llamado relacionado al div
+    div.classList.add("relacionado");
+    div.dataset.id = rel.id;
+
+    div.innerHTML = `
+        <img src="${rel.imagen}" alt="${rel.Titulo}">
+        <p>${rel.Titulo}</p>
+    `;
+
+    //Interactuabilidad: Al hacer clic en un producto relacionado, redirige a su página de detalles
+    div.addEventListener("click", () => {
+        window.location.href = `productos_detalles.html?id=${rel.id}`;
+    });
+
+    relacionadosBarra.appendChild(div);
+}
 
 
 
